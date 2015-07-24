4ba9ea0f772322f261c6a0739ed36f8a0f4fd22a
jasenc <jasen.c8@gmail.com>
2015-07-24 15:38:33 -0700

    feat: add additional content to fresh_tomatoes.py for display on web page

--- fresh_tomatoes.py
+++ fresh_tomatoes.py
@@ -58,6 +58,9 @@ movie_tile_content = '''
 <div class="col-md-6 col-lg-4 movie-tile text-center" data-trailer-youtube-id="{trailer_youtube_id}" data-toggle="modal" data-target="#trailer">
     <img src="{poster_image_url}" width="220" height="342">
     <h2>{movie_title}</h2>
+    <h5>{movie_release}</h5>
+    <h6>{movie_director}</h6>
+    <p>{movie_synopsis}</p>
 </div>
 '''
 
@@ -74,7 +77,12 @@ def create_movie_tiles_content(movies):
         content += movie_tile_content.format(
             movie_title=movie.title,
             poster_image_url=movie.poster_image_url,
-            trailer_youtube_id=trailer_youtube_id
+            trailer_youtube_id=trailer_youtube_id,
+
+            # Adding custom content
+            movie_director = movie.director,
+            movie_release = movie.release_date,
+            movie_synopsis = movie.synopsis
         )
     return content
 
